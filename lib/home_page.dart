cd1788da1bc9220186ed3b6d6a8fd553a19f2cd0
saurav <saurav.maharjan7@gmail.com>
2022-12-28 21:55:52 +0545

    Added BottomNavigation

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,34 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:web_socket_test/screens/chat_page.dart';
+import 'package:web_socket_test/screens/profile.dart';
+import 'package:web_socket_test/screens/rooms.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
+    int currentIndex = 1;
+    Widget screens() {
+      switch (currentIndex) {
+        case 0:
+          return const ChatScreen();
+
+        case 1:
+          return const RoomsScreen();
+
+        case 3:
+          return const ProfileScreen();
+        default:
+          return const ChatScreen();
+      }
+    }
+
+    void updateIndex(int value) {
+      setState(() {
+        currentIndex = value;
+        print(currentIndex);
+      });
+    }
+
     return Scaffold(
-      body: const ChatPage(),
+      body: screens(),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.red,
         unselectedItemColor: Colors.grey.shade600,
         selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
         unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
-        type: BottomNavigationBarType.fixed,
-        items: const [
+        items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.message),
             label: "Chats",
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.group_work),
-            label: "Channels",
+            label: "Rooms",
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.account_box),
             label: "Profile",
           ),
         ],
+        currentIndex: currentIndex,
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
       ),
     );
   }
