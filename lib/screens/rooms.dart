48d945e43840cb94952a1196e9005081b4892796
saurav <saurav.maharjan7@gmail.com>
2023-01-02 00:13:34 +0545

    added SafeArea and button color

--- lib/screens/rooms.dart
+++ lib/screens/rooms.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:web_socket_test/screens/chat_detail_page.dart';
 
 class RoomsScreen extends StatefulWidget {
@@ -16,48 +13,55 @@ class _RoomsScreenState extends State<RoomsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: GridView.builder(
-          physics: NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-          itemCount: 20,
-          itemBuilder: (BuildContext context, int index) {
-            return Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(width: 1),
-                ),
-                padding: EdgeInsets.all(5),
-                margin: EdgeInsets.all(30),
-                // height: MediaQuery.of(context).size.height * 0.3,
-                child: Column(
-                  children: [
-                    Expanded(child: Text(index.toString())),
-                    Expanded(
-                      child: ElevatedButton.icon(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ChatDetailPage(
-                                name: index.toString(),
-                              ),
-                            ),
-                          );
-                        },
-                        icon: Icon(
-                          Icons.meeting_room,
+        child: SafeArea(
+          child: GridView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemCount: 20,
+            itemBuilder: (BuildContext context, int index) {
+              return Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlueAccent,
+                    borderRadius: BorderRadius.circular(10),
+                    // border: Border.all(width: 1),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  margin: const EdgeInsets.all(40),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          index.toString(),
                         ),
-                        label: Text('Enter'),
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ChatDetailPage(
+                                  name: index.toString(),
+                                ),
+                              ),
+                            );
+                          },
+                          icon: const Icon(
+                            Icons.meeting_room,
+                          ),
+                          label: const Text('Enter'),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -69,25 +73,30 @@ class _RoomsScreenState extends State<RoomsScreen> {
                   padding: const EdgeInsets.all(10),
                   height: MediaQuery.of(context).size.height * 0.5,
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Expanded(
+                      const Flexible(
                         child: TextField(
                           decoration: InputDecoration(
-                            border: InputBorder.none,
                             label: Text('Room Name'),
                           ),
                         ),
                       ),
-                      Expanded(
+                      Flexible(
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             ElevatedButton.icon(
+                              style: const ButtonStyle(
+                                backgroundColor:
+                                    MaterialStatePropertyAll(Colors.red),
+                              ),
                               onPressed: () {
                                 Navigator.pop(context);
                               },
-                              icon: const Icon(Icons.cancel_sharp),
+                              icon: const Icon(
+                                Icons.cancel_sharp,
+                              ),
                               label: const Text('Cancel'),
                             ),
                             ElevatedButton.icon(
@@ -103,7 +112,7 @@ class _RoomsScreenState extends State<RoomsScreen> {
                 );
               });
         },
-        child: Icon(
+        child: const Icon(
           Icons.add_outlined,
           size: 35,
         ),
