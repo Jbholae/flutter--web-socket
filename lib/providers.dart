70ac1426be516dfe8f23e905776c1866d46f1170
saurav <saurav.maharjan7@gmail.com>
2023-01-03 10:49:40 +0545

    created provider list and adding providers

---
+++ lib/providers.dart
@@ -0,0 +1,13 @@
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+import 'package:web_socket_test/provider/create_user.dart';
+import 'package:web_socket_test/provider/user_chat_rooms.dart';
+
+List<SingleChildWidget> providers = [
+  ChangeNotifierProvider<CreateUserProvider>(
+    create: (context) => CreateUserProvider(),
+  ),
+  ChangeNotifierProvider<UserChatRoomsProvider>(
+    create: (context) => UserChatRoomsProvider(),
+  ),
+];
