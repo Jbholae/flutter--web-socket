36708e50f756dfd14af71736fe022c0e7012519d
saurav <saurav.maharjan7@gmail.com>
2023-01-03 10:51:11 +0545

    implemented char room provider

--- lib/screens/rooms.dart
+++ lib/screens/rooms.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:web_socket_test/provider/user_chat_rooms.dart';
 import 'package:web_socket_test/screens/chat_detail_page.dart';
 
 class RoomsScreen extends StatefulWidget {
@@ -11,57 +13,66 @@ class RoomsScreen extends StatefulWidget {
 class _RoomsScreenState extends State<RoomsScreen> {
   @override
   Widget build(BuildContext context) {
+    Provider.of<UserChatRoomsProvider>(context).getChatRoom(1);
     return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
-          child: GridView.builder(
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            ),
-            itemCount: 20,
-            itemBuilder: (BuildContext context, int index) {
-              return Center(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.lightBlueAccent,
-                    borderRadius: BorderRadius.circular(10),
-                    // border: Border.all(width: 1),
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  margin: const EdgeInsets.all(40),
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          index.toString(),
-                        ),
-                      ),
-                      Expanded(
-                        child: ElevatedButton.icon(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ChatDetailPage(
-                                  name: index.toString(),
+          child: Consumer<UserChatRoomsProvider>(
+              builder: (context, getRoom, child) {
+            return getRoom.loading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    itemCount: 20,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Center(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlueAccent,
+                            borderRadius: BorderRadius.circular(10),
+                            // border: Border.all(width: 1),
+                          ),
+                          padding: const EdgeInsets.all(10),
+                          margin: const EdgeInsets.all(40),
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  index.toString(),
                                 ),
                               ),
-                            );
-                          },
-                          icon: const Icon(
-                            Icons.meeting_room,
+                              Expanded(
+                                child: ElevatedButton.icon(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => ChatDetailPage(
+                                          name: index.toString(),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                  icon: const Icon(
+                                    Icons.meeting_room,
+                                  ),
+                                  label: const Text('Enter'),
+                                ),
+                              ),
+                            ],
                           ),
-                          label: const Text('Enter'),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
+                      );
+                    },
+                  );
+          }),
         ),
       ),
       floatingActionButton: FloatingActionButton(
