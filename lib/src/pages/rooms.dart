966d4d71fa57fba59c39ec881882bbf695052836
saurav <saurav.maharjan7@gmail.com>
2023-01-09 11:50:40 +0545

    FIX : Added condition to check user room is empty

--- lib/src/pages/rooms.dart
+++ lib/src/pages/rooms.dart
@@ -28,30 +28,34 @@ class _RoomsScreenState extends State<RoomsScreen> {
               if (snapshot.connectionState == ConnectionState.done) {
                 if (snapshot.hasData) {
                   var data = snapshot.data;
-                  return ListView.separated(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    itemCount: data!.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      var chatData = data[index];
-                      return ListTile(
-                        title: Text(
-                          "Name : ${chatData.name}",
-                        ),
-                        leading: const CircleAvatar(
-                          backgroundImage: NetworkImage(
-                              "https://randomuser.me/api/portraits/men/5.jpg"),
-                          maxRadius: 20,
-                        ),
-                        onTap: () {
-                          mainNavigator.currentState
-                              ?.pushNamed("/chat", arguments: index.toString());
-                        },
-                      );
-                    },
-                    separatorBuilder: (BuildContext context, int index) {
-                      return const Divider();
-                    },
-                  );
+                  return data!.isEmpty
+                      ? const Center(
+                          child: Text('No Rooms Found !!!'),
+                        )
+                      : ListView.separated(
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          itemCount: data.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            var chatData = data[index];
+                            return ListTile(
+                              title: Text(
+                                "Name : ${chatData.name}",
+                              ),
+                              leading: const CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    "https://randomuser.me/api/portraits/men/5.jpg"),
+                                maxRadius: 20,
+                              ),
+                              onTap: () {
+                                mainNavigator.currentState?.pushNamed("/chat",
+                                    arguments: index.toString());
+                              },
+                            );
+                          },
+                          separatorBuilder: (BuildContext context, int index) {
+                            return const Divider();
+                          },
+                        );
                 } else if (snapshot.hasError) {
                   return Text(snapshot.error.toString());
                 }
