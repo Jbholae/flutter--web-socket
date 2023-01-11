e5350dbbf2191d524bca9ee8ce869b3859fa526d
saurav <saurav.maharjan7@gmail.com>
2023-01-11 21:31:17 +0545

    feat : firebase initialization

--- lib/src/injector.dart
+++ lib/src/injector.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'services/app_services.dart';
 
@@ -5,7 +6,7 @@ late SharedPreferences sharedPreferences;
 late AppRepoImplementation apiService;
 
 Future<void> initializeDependencies() async {
-  // await Firebase.initializeApp();
+  await Firebase.initializeApp();
   sharedPreferences = await SharedPreferences.getInstance();
   apiService = AppRepoImplementation();
 }
