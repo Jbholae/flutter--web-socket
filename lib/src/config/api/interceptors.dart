b27126523641c9914d4552ed53e5116f1c040c5b
saurav <saurav.maharjan7@gmail.com>
2023-01-12 15:21:25 +0545

    feat : added token header

--- lib/src/config/api/interceptors.dart
+++ lib/src/config/api/interceptors.dart
@@ -1,11 +1,21 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+
+import '../firebase/auth.dart';
 
 class DioAuthInterceptors extends Interceptor {
   @override
   void onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
     debugPrint(options.uri.toString());
+    handler.next(options);
+    String? token = await firebaseAuth.currentUser?.getIdToken();
+    if (token != null) {
+      options.headers
+          .addAll({HttpHeaders.authorizationHeader: "Bearer $token"});
+    }
     handler.next(options);
   }
 }
