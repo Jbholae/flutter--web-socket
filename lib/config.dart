9aa4d854e188acd849da6fd13cc8ea6ef1e0b1bc
saurav <saurav.maharjan7@gmail.com>
2023-01-13 14:20:44 +0545

    fix : changed api

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 class Config {
-  static const apiUrl = "http://192.168.1.69:8000";
+  static const apiUrl = "http://192.168.1.71:5000";
   // static const apiUrl = "http://192.168.1.69:8000";
   // static const apiUrl = "http://localhost:8000";
   static const locale = "en";
