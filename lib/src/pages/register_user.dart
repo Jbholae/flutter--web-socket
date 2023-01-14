bc304fd72a978682595d8ec10155530225e0518d
Sawal Timsina <52816353+sawal-timsina@users.noreply.github.com>
2023-01-14 14:44:35 +0545

    fix: navigate and logout btn

--- lib/src/pages/register_user.dart
+++ lib/src/pages/register_user.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:provider/provider.dart';
 
+import '../app.dart';
 import '../config/firebase/auth.dart';
 import '../injector.dart';
 import '../models/user/user.dart';
+import '../providers/auth_provider.dart';
+import 'login_user.dart';
 
 class RegisterUser extends StatefulWidget {
   const RegisterUser({Key? key}) : super(key: key);
@@ -99,6 +103,22 @@ class _RegisterUserState extends State<RegisterUser> {
                   },
                   child: const Text('Register'),
                 ),
+                Consumer<AuthProvider>(builder: (ctx, value, _) {
+                  if (!value.loggedIn) {
+                    return ElevatedButton(
+                      onPressed: () {
+                        mainNavigator.currentState?.pushReplacementNamed(LoginUser.routeName);
+                      },
+                      child: const Text('Login'),
+                    );
+                  }
+                  return ElevatedButton(
+                    onPressed: () {
+                      firebaseAuth.signOut();
+                    },
+                    child: const Text('Logout'),
+                  );
+                })
               ],
             ),
           ),
