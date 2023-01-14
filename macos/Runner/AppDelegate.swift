114782c7999a632459f7851ed7b5579364c9212d
Sawal Timsina <52816353+sawal-timsina@users.noreply.github.com>
2023-01-14 08:21:33 +0545

    chore: ios firebase

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,8 +1,18 @@
 import Cocoa
 import FlutterMacOS
+import FirebaseCore
+import FirebaseAuth
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
+    func application(_ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+          [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+    
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
