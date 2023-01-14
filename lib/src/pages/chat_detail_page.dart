bc7d65a074439a0779450e898a6e2f02500834e2
Sawal Timsina <52816353+sawal-timsina@users.noreply.github.com>
2023-01-14 08:24:28 +0545

    ui: chat message page

--- lib/src/pages/chat_detail_page.dart
+++ lib/src/pages/chat_detail_page.dart
@@ -1,189 +1,244 @@
-import 'package:flutter/material.dart';
+import 'dart:convert' show jsonDecode, jsonEncode;
+import 'dart:io' show HttpHeaders;
 
+import 'package:flutter/material.dart'
+    show
+        AppBar,
+        BorderRadius,
+        BoxConstraints,
+        BoxDecoration,
+        BuildContext,
+        CircleAvatar,
+        Colors,
+        Column,
+        Container,
+        CrossAxisAlignment,
+        Divider,
+        EdgeInsets,
+        Expanded,
+        FontWeight,
+        Icon,
+        IconButton,
+        Icons,
+        InputDecoration,
+        ListView,
+        NetworkImage,
+        Row,
+        Scaffold,
+        SizedBox,
+        State,
+        StatefulWidget,
+        StreamBuilder,
+        Text,
+        TextDirection,
+        TextEditingController,
+        TextField,
+        TextStyle,
+        Theme,
+        Widget;
+import 'package:flutter/rendering.dart';
+import 'package:provider/provider.dart' show Consumer;
+import 'package:rxdart/rxdart.dart';
+import 'package:web_socket_channel/io.dart' show IOWebSocketChannel;
+
+import '../../config.dart';
+import '../config/firebase/auth.dart';
+import '../injector.dart';
 import '../models/chat_message_model.dart';
+import '../models/rooms/chat_room_model.dart';
+import '../providers/auth_provider.dart';
 
 class ChatDetailPage extends StatefulWidget {
-  const ChatDetailPage({super.key, required this.name});
+  const ChatDetailPage({super.key, required this.room});
 
-  final String name;
+  final ChatRoom room;
 
   @override
   State<ChatDetailPage> createState() => _ChatDetailPageState();
 }
 
 class _ChatDetailPageState extends State<ChatDetailPage> {
-  List<ChatMessage> messages = [
-    ChatMessage(
-      messageContent: "Hello, Will",
-      messageType: "receiver",
-    ),
-    ChatMessage(
-      messageContent: "How have you been?",
-      messageType: "receiver",
-    ),
-    ChatMessage(
-      messageContent: "Hey Kriss, I am doing fine dude. wbu?",
-      messageType: "sender",
-    ),
-    ChatMessage(
-      messageContent: "ehhhh, doing OK.",
-      messageType: "receiver",
-    ),
-    ChatMessage(
-      messageContent: "Is there any thing wrong?",
-      messageType: "sender",
-    ),
-  ];
+  late final IOWebSocketChannel ws;
+  late final TextEditingController message = TextEditingController();
+
+  late BehaviorSubject<List<ChatMessage>> messageStream =
+      BehaviorSubject<List<ChatMessage>>()
+        ..addStream(apiService.getRoomMessages(widget.room.id).asStream());
+
+  @override
+  void initState() {
+    String baseUrl =
+        Config.apiUrl.replaceRange(0, Config.apiUrl.indexOf("/") + 2, "");
+    firebaseAuth.currentUser?.getIdToken().then((value) {
+      setState(() {
+        ws = IOWebSocketChannel.connect(
+          'ws://$baseUrl/rooms/chat/${widget.room.id}',
+          headers: {HttpHeaders.authorizationHeader: "Bearer $value"},
+        );
+
+        ws.stream.listen((event) {
+          messageStream.add([
+            ChatMessage.fromJson(jsonDecode(event)),
+            ...messageStream.value,
+          ]);
+        });
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    messageStream.close();
+    ws.innerWebSocket?.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        automaticallyImplyLeading: false,
+        titleSpacing: 0,
         backgroundColor: Colors.white,
-        flexibleSpace: SafeArea(
-          child: Container(
-            padding: const EdgeInsets.only(right: 16),
-            child: Row(
+        title: Row(
+          children: [
+            const CircleAvatar(
+              backgroundImage:
+                  NetworkImage("https://randomuser.me/api/portraits/men/5.jpg"),
+              maxRadius: 18,
+            ),
+            const SizedBox(width: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.room.name,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w600),
+                ),
+                Text(
+                  "Online",
+                  style: TextStyle(color: Colors.grey.shade600, fontSize: 13),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      body: StreamBuilder<List<ChatMessage>>(
+          stream: messageStream,
+          builder: (context, snapshot) {
+            final messages = snapshot.data ?? [];
+            return Column(
               children: <Widget>[
-                IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(
-                    Icons.arrow_back,
-                    color: Colors.black,
+                Expanded(
+                  child: ListView.separated(
+                    reverse: true,
+                    itemCount: messages.length,
+                    padding:
+                        const EdgeInsets.only(bottom: 8, left: 12, right: 12),
+                    itemBuilder: (context, index) {
+                      final message = messages[index];
+                      return Consumer<AuthProvider>(
+                          builder: (context, value, _) {
+                        final isUser = value.user!.uid == message.userId;
+                        return Row(
+                          textDirection:
+                              isUser ? TextDirection.rtl : TextDirection.ltr,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            if (!isUser) ...[
+                              const CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    "https://randomuser.me/api/portraits/men/7.jpg"),
+                                maxRadius: 12,
+                              ),
+                              const SizedBox(width: 8),
+                            ],
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: !isUser
+                                    ? Colors.grey[800]
+                                    : Colors.blue[400],
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 8),
+                              child: Text(
+                                message.text.trim(),
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium
+                                    ?.copyWith(color: Colors.white),
+                              ),
+                            )
+                          ],
+                        );
+                      });
+                    },
+                    separatorBuilder: (BuildContext context, int index) {
+                      return const Divider(
+                        height: 2,
+                        thickness: 0,
+                        color: Colors.transparent,
+                      );
+                    },
                   ),
                 ),
-                const SizedBox(
-                  width: 2,
-                ),
-                const CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      "https://randomuser.me/api/portraits/men/5.jpg"),
-                  maxRadius: 20,
-                ),
-                const SizedBox(
-                  width: 12,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 4, vertical: 8),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: <Widget>[
-                      Text(
-                        widget.name,
-                        style: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w600),
+                      IconButton(
+                        icon: const Icon(Icons.add),
+                        color: Colors.blue[400],
+                        constraints:
+                            const BoxConstraints(minHeight: 16, minWidth: 16),
+                        splashRadius: 18,
+                        onPressed: () {},
                       ),
-                      const SizedBox(
-                        height: 6,
+                      // const SizedBox(width: ),
+                      Expanded(
+                        child: TextField(
+                          controller: message,
+                          minLines: 1,
+                          maxLines: 6,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                          decoration: InputDecoration(
+                            isDense: true,
+                            hintText: "Aa",
+                            fillColor: Colors.grey[200],
+                            contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 8),
+                          ),
+                        ),
                       ),
-                      Text(
-                        "Online",
-                        style: TextStyle(
-                            color: Colors.grey.shade600, fontSize: 13),
+                      const SizedBox(width: 8),
+                      IconButton(
+                        icon: const Icon(Icons.send, size: 20),
+                        color: Colors.blue[400],
+                        constraints:
+                            const BoxConstraints(minHeight: 16, minWidth: 16),
+                        splashRadius: 18,
+                        onPressed: () {
+                          final text = message.text.trim();
+                          if (text != "") {
+                            ws.innerWebSocket!.add(jsonEncode({
+                              "text": message.text.trim(),
+                            }));
+                            message.clear();
+                          }
+                        },
                       ),
                     ],
                   ),
                 ),
-                const Icon(
-                  Icons.settings,
-                  color: Colors.black54,
-                ),
               ],
-            ),
-          ),
-        ),
-      ),
-      body: Stack(
-        children: <Widget>[
-          ListView.builder(
-            itemCount: messages.length,
-            shrinkWrap: true,
-            padding: const EdgeInsets.only(top: 10, bottom: 10),
-            physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.only(
-                    left: 14, right: 14, top: 10, bottom: 10),
-                child: Align(
-                  alignment: (messages[index].messageType == "receiver"
-                      ? Alignment.topLeft
-                      : Alignment.topRight),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: (messages[index].messageType == "receiver"
-                          ? Colors.grey.shade200
-                          : Colors.blue[200]),
-                    ),
-                    padding: const EdgeInsets.all(16),
-                    child: Text(
-                      messages[index].messageContent!,
-                      style: const TextStyle(fontSize: 15),
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Container(
-              padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
-              height: 60,
-              width: double.infinity,
-              color: Colors.white,
-              child: Row(
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      height: 30,
-                      width: 30,
-                      decoration: BoxDecoration(
-                        color: Colors.lightBlue,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: const Icon(
-                        Icons.add,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  const Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                          hintText: "Write message...",
-                          hintStyle: TextStyle(color: Colors.black54),
-                          border: InputBorder.none),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {},
-                    backgroundColor: Colors.blue,
-                    elevation: 0,
-                    child: const Icon(
-                      Icons.send,
-                      color: Colors.white,
-                      size: 18,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+            );
+          }),
     );
   }
 }
