5c0bd8b5e28f39c4bd17c54402f5148e9aeca575
Sawal Timsina <52816353+sawal-timsina@users.noreply.github.com>
2023-01-15 16:47:22 +0545

    fix: added message status

--- lib/src/models/chat_message_model.dart
+++ lib/src/models/chat_message_model.dart
@@ -1,36 +1,58 @@
 import 'user/user.dart';
 
 class ChatMessage {
-  int id;
+  int? id;
   String text;
   String userId;
   int roomId;
-  User user;
+  User? user;
   String? createdAt;
   String? updatedAt;
   String? deletedAt;
+  String status;
 
   ChatMessage({
-    required this.id,
+    this.id,
+    required this.status,
     required this.text,
     required this.roomId,
     required this.userId,
-    required this.user,
-    this.deletedAt,
-    this.updatedAt,
+    this.user,
     this.createdAt,
-  });
+    this.updatedAt,
+    this.deletedAt,
+  }) {
+    createdAt = createdAt ?? DateTime.now().toUtc().toIso8601String();
+    updatedAt = updatedAt ?? createdAt;
+  }
 
   factory ChatMessage.fromJson(Map<String, dynamic> json) {
     return ChatMessage(
-      id: json['id'] as int,
+      id: json['id'],
       text: json['text'] as String,
+      status: json['status'] ?? "sent",
       userId: json['user_id'] as String,
       roomId: json['room_id'] as int,
       createdAt: json['created_at'] as String?,
       updatedAt: json['updated_at'] as String?,
       deletedAt: json['deleted_at'] as String?,
-      user: User.fromJson(json['user']),
+      user: json['user'] != null
+          ? User.fromJson(json['user'])
+          : User(id: 0, fullName: "", email: ''),
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "text": text,
+      "status": status,
+      "user_id": userId,
+      "room_id": roomId,
+      "user": user?.toJson() ?? {},
+      "created_at": createdAt ?? DateTime.now().toUtc().toIso8601String(),
+      "updated_at": updatedAt ?? DateTime.now().toUtc().toIso8601String(),
+      "deleted_at": deletedAt,
+    };
   }
 }
