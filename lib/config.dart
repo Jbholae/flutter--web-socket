3b8e6bd69682acfd1c1c208a716f88c3dc14107e
saurav <saurav.maharjan7@gmail.com>
2023-01-19 18:20:11 +0545

    fix : api url

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
-  static const apiUrl = "http://192.168.1.68:5000";
-  static const socketUrl = "ws://192.168.1.68:5000";
+  static const apiUrl = "http://192.168.1.72:5000";
+  static const socketUrl = "ws://192.168.1.72:5000";
   // static const apiUrl = "http://192.168.1.69:8000";
   // static const apiUrl = "http://localhost:8000";
   static const locale = "en";
