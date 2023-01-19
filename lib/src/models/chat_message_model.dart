b2d9a1ef81dd071ef323d36e9cb7d6db6b1e783d
saurav <saurav.maharjan7@gmail.com>
2023-01-19 18:20:46 +0545

    fix : refactor, change in datatypes

--- lib/src/models/chat_message_model.dart
+++ lib/src/models/chat_message_model.dart
@@ -1,34 +1,29 @@
-
 class ChatMessage {
-  String? id;
+  int? id;
   String? text;
-  String? userRoomId;
-  DateTime? createdAt;
-  DateTime? updatedAt;
-  DateTime? deletedAt;
-  // String? messageContent;
-  // String? messageType;
+  int? userRoomId;
+  String? userId;
   ChatMessage({
-    // this.messageContent,
-    // this.messageType,
-    this.createdAt,
-    this.deletedAt,
     this.id,
     this.text,
-    this.updatedAt,
     this.userRoomId,
+    this.userId,
   });
 
   factory ChatMessage.fromJson(Map<String, dynamic> json) {
     return ChatMessage(
-      createdAt: json['created_at'],
-      deletedAt: json['deleted_at'],
-      id: json['id'],
-      // messageContent: json['message_content'],
-      // messageType: json['message_type'],
-      text: json['text'],
-      updatedAt: json['updated_at'],
-      userRoomId: json['user_room_id'],
-    );
+        id: json['id'],
+        // messageContent: json['message_content'],
+        // messageType: json['message_type'],
+        text: json['text'],
+        userRoomId: json['room_id'],
+        userId: json['user_id']);
   }
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "text": text,
+        "room_id": userRoomId,
+        "user_id": userId,
+      };
 }
