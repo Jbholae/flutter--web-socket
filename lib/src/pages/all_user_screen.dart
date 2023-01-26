3a5bea91ef7f8a3eb778654c1356046a5541454c
saurav <saurav.maharjan7@gmail.com>
2023-01-26 18:47:15 +0545

    feat : UI for search user

--- lib/src/pages/all_user_screen.dart
+++ lib/src/pages/all_user_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 import '../injector.dart';
 import '../models/user/get_all_user_response.dart/get_all_user_response.dart';
@@ -11,45 +14,95 @@ class AllUserScreen extends StatefulWidget {
 }
 
 class _AllUserScreenState extends State<AllUserScreen> {
+  TextEditingController searchController = TextEditingController();
+
+  Future<List<GetAllUserResponseData>>? userData;
+
+  @override
+  void initState() {
+    loadData(searchController.text);
+    super.initState();
+  }
+
+  loadData(String? keyword) async {
+    userData = apiService.getAllUser(keyword: keyword);
+    return userData;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: FutureBuilder<List<GetAllUserResponseData>>(
-            future: apiService.getAllUser(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasData) {
-                  var data = snapshot.data;
-                  return data!.isEmpty
-                      ? const Center(
-                          child: Text('No User Found !!!'),
-                        )
-                      : ListView.builder(
-                          itemCount: data.length,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              leading: const CircleAvatar(
-                                backgroundImage: NetworkImage(
-                                    "https://randomuser.me/api/portraits/men/4.jpg"),
-                                maxRadius: 20,
-                              ),
-                              title: Text(data[index].fullName!),
-                              subtitle: Text(data[index].email!),
-                              trailing: Text('Add'),
-                            );
-                          },
+        child: Column(
+          children: [
+            Flexible(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: FormBuilderTextField(
+                  textInputAction: TextInputAction.search,
+                  name: "search",
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    hintText: "Search for users",
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        searchController.clear();
+                      },
+                      icon: const Icon(Icons.close),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    loadData(value!);
+                    setState(() {
+                      userData = apiService.getAllUser(keyword: value);
+                    });
+                    print("text : ${searchController.text}");
+                    print("text : ${value}");
+                  },
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 10,
+              child: FutureBuilder<List<GetAllUserResponseData>>(
+                  future: userData,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasData) {
+                        var data = snapshot.data;
+                        return data!.isEmpty
+                            ? const Center(
+                                child: Text('No User Found !!!'),
+                              )
+                            : ListView.builder(
+                                itemCount: data.length,
+                                itemBuilder: (context, index) {
+                                  return ListTile(
+                                    leading: const CircleAvatar(
+                                      backgroundImage: NetworkImage(
+                                          "https://randomuser.me/api/portraits/men/4.jpg"),
+                                      maxRadius: 20,
+                                    ),
+                                    title: Text(data[index].fullName!),
+                                    subtitle: Text(data[index].email!),
+                                    trailing: Text('Add'),
+                                  );
+                                },
+                              );
+                      } else if (snapshot.hasError) {
+                        return Center(
+                          child: Text(snapshot.error.toString()),
                         );
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text(snapshot.error.toString()),
-                  );
-                }
-              }
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }),
+                      }
+                    }
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
